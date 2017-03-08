7e5198f78b854d2d99c69b5fa0c36e8513648317
Anup Kishore <anup@secondspectrum.com>
2017-03-08 10:17:06 -0800

    Part 1, day 4

--- p4/main.hs
+++ p4/main.hs
@@ -1,15 +1,36 @@
 import Data.List.Split
 import Data.List
+import Data.Function((&))
 
 data Room = Room String Integer [Char] deriving Show
 -- room :: String -> [[String]]
 room x = do
   let code' : sect' : name' = endByOneOf "[-" . reverse $ x
-  let [name, sect, code] = map reverse [concat name', sect, drop 1 code']
+  let [name, sect, code] = map reverse [concat name', sect', drop 1 code']
   Room name (read sect :: Integer) code
+
+sorter (l, a) (l2, a2)
+  | l < l2 = GT
+  | l > l2 = LT
+  | l == l2 = compare a a2
+
+valid (Room name _ code) =
+  name
+  & sort
+  & group
+  & map(\x -> (length x, head x))
+  & sortBy sorter
+  & map snd
+  & take 5
+  & (== code)
+
+sector :: Room -> Integer
+sector (Room _ s _) = s
 
 main = do
   file <- readFile "./input"
   let file' = intercalate "\n" ["aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]", "totally-real-room-200[decoy]"]
-  let rooms = map room . lines $ file'
-  print $ take 3 rooms
+  let rooms = map room . lines $ file
+  let validRooms = map fst . filter snd . zip rooms . map valid $ rooms 
+  putStrLn $ (++) "Part 1: " . show . foldl (+) 0 . map sector $ validRooms
+  -- putStrLn $ (++) "Part 2: " . show . foldl (+) 0 . map sector $ validRooms
