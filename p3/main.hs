ed02a1036cb7988360f9d2cd97a24bfb0abf571a
Anup Kishore <anup@secondspectrum.com>
2017-03-08 08:59:00 -0800

    Fixing p3

--- p3/main.hs
+++ p3/main.hs
@@ -6,14 +6,18 @@ int x = read x :: Integer
 
 isTriangle :: [Integer] -> Bool
 isTriangle [x, y, z] = (x + y) > z
+isTriangle _ = False
 
 triangle :: [String] -> Bool
 triangle x = isTriangle . sort . map int $ x
+
+rotateRight :: [[String]] -> [[String]]
+rotateRight = (transpose . reverse)
 
 main = do
   file <- readFile "./input"
   let file' = "101 301 501\n102 302 502\n103 303 503\n201 401 601\n202 402 602\n203 403 603"
   let triangles = map words . lines $ file
-  let triangles' = concat . map (transpose . reverse) . chunksOf 3 $ triangles
-  putStrLn $ (++)"Part 1 " . show . length . filter id . map triangle $ triangles
-  putStrLn $ (++)"Part 2 " . show . length . filter id . map triangle $ triangles'
+  let triangles' = concat . map rotateRight . chunksOf 3 $ triangles
+  putStrLn $ (++) "Part 1 " . show . length . filter id . map triangle $ triangles
+  putStrLn $ (++) "Part 2 " . show . length . filter id . map triangle $ triangles'
