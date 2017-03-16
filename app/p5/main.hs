16eb170ca6956731f910c9832d2d3fbf51947390
Anup Kishore <anup@secondspectrum.com>
2017-03-16 01:57:04 -0700

    Part 2, Day 5, shitty naming

--- app/p5/main.hs
+++ app/p5/main.hs
@@ -6,7 +6,9 @@ import qualified Data.ByteString.Lazy.Char8 as L(concat)
 import qualified Data.ByteString as BS
 import Data.ByteString.Lazy.Internal (ByteString)
 import Data.Word
-import Data.Bits((.&.))
+import Data.Bits((.&.), shift)
+import Data.List(nubBy, sortBy)
+import Data.Ord(comparing)
 
 value :: String -> Int -> String
 value s = (++) s . show
@@ -27,16 +29,21 @@ getInput :: [Int] -> String -> [(Int, String)]
 getInput i f = zip i . repeat $ f
 first8 = take 8 . filter valid
 pass (0:0:x:_) = (toLazyByteString . word8Hex) $ x
+pass2 (0:0:_:x:_) = (toLazyByteString . word8Hex) $ shift (x .&. 0xf0) (-4)
+
+eigth (0:0:x:_) = x
 
 hashes i = filter valid . map process . getInput i
 
 p1 :: [Int] -> String -> ByteString
 p1 i = L.concat . map pass . take 8 . hashes i
 
+p2 :: [Int] -> String -> ByteString
+p2 i = L.concat . map pass2 . sortBy (comparing eigth) . take 8 . filter (\x -> eigth x < 8) . nubBy (\x y -> eigth x == eigth y) . hashes i
+
 main = do
   let file  = "reyedfim"
   let file' = "abc"
   let ints  = [0..]
-  let ints' = [3231929, 5017308, 5278568]
-  print $ p1 ints file
-  
+  let ints' = [3231929, 5017308, 5278568, 5357525]
+  print $ p2 ints file
