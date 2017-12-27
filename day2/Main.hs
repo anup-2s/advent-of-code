ae5b8a24599dce03e11e8bc1a8e666ca44eae8a5
Anup Kishore <anup@secondspectrum.com>
2017-12-27 14:12:18 -0500

    Take that, day 2

---
+++ day2/Main.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Arrow ((&&&))
+import Data.Char (isSpace)
+import Data.Text (split, pack, unpack)
+import Paths_aoc2017
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
+
+file :: IO FilePath
+file = getDataFileName "data/2-1"
+
+wordToInt :: String -> Int
+wordToInt = read
+
+getData :: IO [[Int]]
+getData = map (map (wordToInt . unpack) . split isSpace . pack) . lines <$> (readFile =<< file)
+
+usage :: IO ()
+usage = putStrLn "Usage: stack exec day-n [--two] [...args]"
+
+maxMinDiff :: [Int] -> Int
+maxMinDiff = uncurry (-) . (maximum &&& minimum)
+
+doOne :: [String] -> IO Int
+doOne _ = sum . map maxMinDiff <$> getData
+
+findDivisors :: [Int] -> Int -- partial
+findDivisors xs = head [a `div` b | a <- xs, b <- xs, a /= b, a `mod` b == 0]
+
+doTwo :: [String] -> IO Int
+doTwo _ = sum . map findDivisors <$> getData
+
+parse :: [String] -> IO ()
+parse ("--two":xs) = print =<< doTwo xs
+parse ["--help"] = usage
+parse xs = print =<< doOne xs
+
+main :: IO ()
+main = do
+  args <- getArgs
+  parse args
+  exitSuccess
