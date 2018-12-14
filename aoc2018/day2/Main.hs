ab1ef4b84734f0c5e4bf2ebac60fdb990cdd75f9
Anup Kishore <anup@secondspectrum.com>
2018-12-14 13:50:58 -0800

    Complete 2018/2.1

--- aoc2018/day2/Main.hs
+++ aoc2018/day2/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
-import           Data.Text (Text, unpack)
-import           Lib       (readDataFile)
+import           Control.Arrow ((&&&))
+import qualified Data.List     as List
+import           Data.Set      (Set, fromList)
+import qualified Data.Set      as Set
+import           Data.Text     (Text, unpack)
+import           Lib           (readDataFile)
 
 data Error =
   NoElementsSupplied
@@ -14,7 +18,14 @@ parse = safeHd . lines . unpack
     safeHd (x:xs) = Right (x, xs)
 
 p1 :: (String, [String]) -> Int
-p1 = undefined
+p1 =
+  uncurry (*) .
+  (count (Set.member 3) &&& count (Set.member 2)) . counts . uncurry (:)
+  where
+    counts :: [String] -> [Set Int]
+    counts = map (fromList . map length . List.group . List.sort)
+    count :: (a -> Bool) -> [a] -> Int
+    count cond = length . filter cond
 
 main :: IO ()
 main = print . fmap p1 . parse =<< readDataFile "data/day2/p1"
